dfc6e883fe977e02c62db63564947f54d1e0a1c7
Eric Dejonckheere <eric@aya.io>
2015-06-27 16:04:42 +0200

    Initial Commit

---
+++ testPok/AppDelegate.swift
@@ -0,0 +1,27 @@
+//
+//  AppDelegate.swift
+//  testPok
+//
+//  Created by ERIC DEJONCKHEERE on 27/06/2015.
+//  Copyright (c) 2015 Eric Dejonckheere. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+    @IBOutlet weak var window: NSWindow!
+
+
+    func applicationDidFinishLaunching(aNotification: NSNotification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(aNotification: NSNotification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
