c70ff5f27a7761c140f3a834f94255d2bee73745
Eric Dejonckheere <eric@aya.io>
2015-08-05 14:08:40 +0200

    added nsdate for dealt hand

--- SwiftyPokerHands/SPHModelCard.swift
+++ SwiftyPokerHands/SPHModelCard.swift
@@ -1,3 +1,5 @@
+import Foundation
+
 struct Card {
 
     let suit: String
--- SwiftyPokerHands/SPHModelPlayer.swift
+++ SwiftyPokerHands/SPHModelPlayer.swift
@@ -1,3 +1,5 @@
+import Foundation
+
 struct Player: CanTakeCard {
 
     init() {}
@@ -8,7 +10,7 @@ struct Player: CanTakeCard {
 
     var name: String?
 
-    var historyOfDealtHoldemCards = [(Card, Card)]()
+    var historyOfDealtHoldemCards = [(Card, Card, NSDate)]()
     
     var frequentHands = [String:Int]()
 
@@ -34,7 +36,7 @@ struct Player: CanTakeCard {
 
     var cards = [Card]() {
         didSet {
-            let tu = (self.cards[0], self.cards[1])
+            let tu = (self.cards[0], self.cards[1], NSDate())
             historyOfDealtHoldemCards.append(tu)
             let fqname = "\(tu.0.description),\(tu.1.description)"
             if frequentHands[fqname] == nil {
